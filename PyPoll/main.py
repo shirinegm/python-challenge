358800f6615b3953f3253253111b9f691f5c6385
Shi Mas <shirinegm@gmail.com>
2021-09-22 23:43:13 -0500

    started work on PyPoll. Establishing calculations for fields

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,24 @@ import os
 #import library for csv imports
 import csv
 
+#declare variables
+total_votes = 0
+candidates_list = []
+index = 0
+
 #store the file using the file location
-file_path = os.paht.join("..", "Resources", "election_data.csv")
+file_path = os.path.join("Resources", "election_data.csv")
 with open(file_path) as csv_file:
-    csv_reader = csv.reader(csf_file)
+    csv_reader = csv.reader(csv_file)
+    csv_header = next(csv_file)
+
+    for row in csv_reader:
+        index = index + 1
+        total_votes = total_votes + 1
+        current_candidate = row[2]
+        if current_candidate not in candidates_list:
+            candidates_list.append(current_candidate)
+
+
+print(total_votes)
+print(candidates_list)
