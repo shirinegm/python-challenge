6e4c31d446f1f7a9232f038e40042e38ce25a658
Shi Mas <shirinegm@gmail.com>
2021-09-23 02:03:18 -0500

    Fixed bug returning only the top two winners

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,8 +6,12 @@ import csv
 
 #declare variables
 total_votes = 0
-candidates_list = []
-index = 0
+candidates = []
+vote_list = []
+candidate_list = []
+votes = []
+percentage = []
+
 
 #store the file using the file location
 file_path = os.path.join("Resources", "election_data.csv")
@@ -15,13 +19,34 @@ with open(file_path) as csv_file:
     csv_reader = csv.reader(csv_file)
     csv_header = next(csv_file)
 
+    # Iterate through the file to create candidates list
     for row in csv_reader:
-        index = index + 1
-        total_votes = total_votes + 1
-        current_candidate = row[2]
-        if current_candidate not in candidates_list:
-            candidates_list.append(current_candidate)
+        candidates.append(row[2])
+    
+    # Calculate total votes
+    total_votes = len(candidates)
+
+    # Get unique list of candidates
+    candidate_list = set(candidates)
+
+    # Count number of votes for each candidate
+    for candidate in candidate_list:
+        votes = candidates.count(candidate)
+        vote_list.append(votes)
+        percentage.append((votes/total_votes)*100)
+
+candidate_list = list(candidate_list)
+
+for vote in vote_list:
+    top_vote = max(vote_list)
+    winner_index = vote_list.index(top_vote)
+    print(f"{candidate_list[winner_index]}: {percentage[winner_index]:3f}% ({top_vote})")
+    vote_list[winner_index] = 0
+    #del candidate_list[winner_index]
+    #del percentage[winner_index]
+    #print(candidate_list)
+    # print(percentage)
+
+    
 
 
-print(total_votes)
-print(candidates_list)
